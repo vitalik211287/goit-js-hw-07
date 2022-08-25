f5f5f456135693371fb748298df5b520bf8ea9ec
vitalik poliakov <vitalik211287@gmail.com>
2022-08-25 16:46:05 +0300

    modal

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,11 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
+const { create, visible } = window.basicLightbox;
+// Change code below this line
 
 // создаю разметку
 
+console.log(create, visible);
 const oneGallery = createGallary(galleryItems);
 
 function createGallary(galleryItems) {
@@ -25,21 +27,44 @@ function createGallary(galleryItems) {
 }
 // рендерю
 
-const eddItemsToGallery = document.querySelector(".gallery");
-eddItemsToGallery.insertAdjacentHTML("beforeend", oneGallery);
+const addItemsToGallery = document.querySelector(".gallery");
+addItemsToGallery.insertAdjacentHTML("beforeend", oneGallery);
+
+//модалка
 
 //добавляю слушателя при клике
- const xxx = document.querySelector("a");
+const xxx = document.querySelector("a");
 
-eddItemsToGallery.addEventListener("click", (event) => {
+addItemsToGallery.addEventListener("click", (event) => {
   event.preventDefault();
-  const targetEl = event.target;
-  const parentTargetEl = targetEl.closest(".gallery__link");
-  console.log(parentTargetEl);
-    if (!parentTargetEl) {
-      return;
-    }
+
+  //     const targetEl = event.target;
+
+  //     // const newImage =
+
+  //   const modalRef = document.createElement("div");
+
+  const instance = create(
+    <div class="gallery__iteml">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </div>
+  );
+  instance.show();
+  console.log(instance);
+
+  //   const parentTargetEl = targetEl.closest(".gallery__link");
+  //   console.log(parentTargetEl);
+  //   if (!parentTargetEl) {
+  //     return;
+  //   }
+  const xxx = event.target.dataset.source;
+  console.log(xxx);
   //    xxx.classList.toggle("gallery__link:hover");
-    console.log(event.target);
+  console.log(event.target);
   //   console.log(event.currentTarget);
 });
