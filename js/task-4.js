d2e41b7094ee80a0637a665a3d74a6ad2f3ccd28
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2025-10-01 22:40:37 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
 
+const handlerTargetFormSubmit = (event) => {
+  event.preventDefault();
+  const { email, password } = formEl.elements;
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+  const data = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(data);
+  formEl.reset();
+};
+
+formEl.addEventListener("submit", handlerTargetFormSubmit);
